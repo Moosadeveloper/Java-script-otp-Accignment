37a1b43a4e5ca9019422f9b18d1834090d71e350
Moosadeveloper <janmoosa77@gmail.com>
2024-06-20 20:18:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function see(length){
+    w=""
+    for(i=0;i<length; i++){
+        w+=Math.floor(Math.random()*10).toString()
+
+    }
+    alert(w);
+}
